adff061829f9a93a3dc77f7fff5128ef54e8de64
moony <moony@hellomouse.net>
2024-04-24 21:43:46 -0500

    Initial commit, build system stuff.

---
+++ WORKSPACE.md
@@ -0,0 +1,2 @@
+Due to limitations of cargo workspaces, there is no single workspace here.
+Instead, platforms get their own workspace(s).
---
+++ x86/target/debug/build/compiler_builtins-02672c9014ea754a/build_script_build-02672c9014ea754a.d
@@ -0,0 +1,5 @@
+/home/moony/kos/x86/target/debug/build/compiler_builtins-02672c9014ea754a/build_script_build-02672c9014ea754a: /home/moony/.cargo/registry/src/index.crates.io-6f17d22bba15001f/compiler_builtins-0.1.109/build.rs
+
+/home/moony/kos/x86/target/debug/build/compiler_builtins-02672c9014ea754a/build_script_build-02672c9014ea754a.d: /home/moony/.cargo/registry/src/index.crates.io-6f17d22bba15001f/compiler_builtins-0.1.109/build.rs
+
+/home/moony/.cargo/registry/src/index.crates.io-6f17d22bba15001f/compiler_builtins-0.1.109/build.rs:
