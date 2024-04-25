cff11209cff3ba1d917fbe2d11a9eb500d26e37a
moony <moony@hellomouse.net>
2024-04-25 00:02:28 -0500

    It boots.

--- x86_64/x86_64_limine/src/main.rs
+++ x86_64/x86_64_limine/src/main.rs
@@ -14,19 +14,6 @@ static FRAMEBUFFER_REQUEST: FramebufferRequest = FramebufferRequest::new();
 unsafe extern "C" fn _limine_entry() -> ! {
     assert!(BASE_REVISION.is_supported());
 
-    if let Some(framebuffer_response) = FRAMEBUFFER_REQUEST.get_response() {
-        if let Some(framebuffer) = framebuffer_response.framebuffers().next() {
-            for i in 0..100_u64 {
-                // Calculate the pixel offset using the framebuffer information we obtained above.
-                // We skip `i` scanlines (pitch is provided in bytes) and add `i * 4` to skip `i` pixels forward.
-                let pixel_offset = i * framebuffer.pitch() + i * 4;
-
-                // Write 0xFFFFFFFF to the provided pixel offset to fill it white.
-                *(framebuffer.addr().add(pixel_offset as usize) as *mut u32) = 0xFFFFFFFF;
-            }
-        }
-    }
-
     kos_kernel::halt();
 }
 
